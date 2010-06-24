c0544ef6072270b5dfee95a254b5770f045d6238
mifan <mifanxp@gmail.com>
2010-06-24 17:30:53 +0800

    seek all page

--- tianya.rb
+++ tianya.rb
@@ -6,9 +6,12 @@ require 'uri'
 require 'pp'
 
 
-USER_NAME = '铁匠smith'
+#USER_NAME = '铁匠smith'
+USER_NAME = '五峰樵夫'
+#USER_NAME = '不见得1'
 
 
+#Retrive User ID form tianya
 class TianyaUID
   include HTTParty
   no_follow true
@@ -34,18 +37,28 @@ class TianyaUID
   end
 end
 
-
+#get 
 class TianyaArticle
   include HTTParty
   no_follow true
   base_uri 'http://my.tianya.cn/i/getUserArticleDo.jsp'
+  #last url: http://my.tianya.cn/i/getUserArticleDo.jsp?userName=%E9%93%81%E5%8C%A0smith&url=http://m.www.bbs.hk.ty/feeds/apps/my/pubReply/9870517/937.shtml&type=4
 
   def initialize(username)
     @username = username
     @userid = TianyaUID.new(USER_NAME).userid
   end
 
-  def article_links(date = nil)
+  def page_articles(page_no)
+    get_response = self.class.get('', :query => {:userName => @username, :url => "http://m.www.bbs.hk.ty/feeds/apps/my/pubReply/#{@userid}/#{page_no}.shtml", :type => 4 } )
+
+    URI.extract(get_response.body,'http').uniq.delete_if do |a|
+      (/^http\:\/\/\w+\.tianya\.cn\/publicforum\/content/ =~ a) == nil
+    end
+
+  end
+
+  def recent_articles(date = nil)
     get_response = self.class.get('', :query => {:userName => @username, :idWriter => @userid, :type => 4 } )
 
     URI.extract(get_response.body,'http').uniq.delete_if do |a|
@@ -54,10 +67,46 @@ class TianyaArticle
 
   end
 
+
+
+
+  def max_list_no
+    get_response = self.class.get('', :query => {:userName => @username, :idWriter => @userid, :type => 4 } )
+
+    page_links = URI.extract(get_response.body,'http').uniq.delete_if do |a|
+      (/^http\:\/\/m\.www\.bbs\.hk\.ty\/feeds\/apps\/my\/pubReply/ =~ a) == nil
+    end
+
+    page_links_no = page_links.collect do |l|
+      l.split("/#{@userid}\/").last.split('.').first.to_i
+    end
+    page_links_no.sort.last
+  end
+
+
+  def all_article_links
+     list_no = max_list_no
+
+    
+  end
+
 end
+
+
+
+
+
  
 article  = TianyaArticle.new(USER_NAME)
 
 
-puts article.article_links
+max_no = article.max_list_no
+puts "max_no:#{max_no}"
+(1..max_no).each do |i|
+  links = article.page_articles(max_no-i+1)
+  puts "page #{max_no-i+1}....."
+  puts links
+  puts "\n"
+  sleep 1
+end
 
